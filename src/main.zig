86f59946c94f72507abc4b901228dd5ebdd8c362
Tilo K <tiloklarenbeek@gmail.com>
2025-10-12 11:57:42 +0200

    feat: use arena allocator

--- src/main.zig
+++ src/main.zig
@@ -10,9 +10,13 @@ var accessCache: ?cache.AccessCache = null;
 var authData: []const u8 = "YWRtaW46YWRtaW4=";
 
 fn on_request(r: zap.Request) !void {
-    var allocator = std.heap.GeneralPurposeAllocator(.{}){};
-    const alloc = allocator.allocator();
-    defer _ = allocator.deinit();
+    var gpa = std.heap.DebugAllocator(.{ .retain_metadata = true }){};
+    defer _ = gpa.deinit();
+
+    var arena = std.heap.ArenaAllocator.init(gpa.allocator());
+    defer arena.deinit();
+
+    const alloc = arena.allocator();
 
     if (r.path) |the_path| {
         if (std.mem.eql(u8, the_path, "/") | std.mem.eql(u8, the_path, "")) {
